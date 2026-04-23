f012a5bf218d05366bdeb28fabef7fd3c75152fe
Jaswinder Singh <jaswindersingh79724@gmail.com>
2026-04-23 17:30:01 +0530

    .env variables update

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://medicin-donation.onrender.com",
+  baseURL: import.meta.env.VITE_BASE_URL
 });
 
 API.interceptors.request.use((req) => {
