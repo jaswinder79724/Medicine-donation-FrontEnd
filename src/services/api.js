5547b3b2068f87e52fd89812078056a62b28bba1
Jaswinder Singh <jaswindersingh79724@gmail.com>
2026-04-23 15:12:21 +0530

    online base url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: "https://medicin-donation.onrender.com",
 });
 
 API.interceptors.request.use((req) => {
